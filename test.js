3dca4edf17c784ec06c8f65dc7f79abe6b7c44a0
Nicolaas / Sunn Side Up <github@sunnysideup.co.nz>
2022-05-01 12:53:02 +1200

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,11 @@
 let x = 0
-console.log('START')
+console.log('start to count ...')
 let startTime = Date.now()
 while (x < 1000000000) {
   x++
 }
+
+console.log('Counted to ' + x)
+
 let endTime = Date.now()
 console.log(`Call to count to one billion took ${(endTime - startTime) / 1000} seconds`)
